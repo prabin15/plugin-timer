606360a17fd13e75cb0d8407c09dabd76fe7f069
prabin15 <71572281+prabin15@users.noreply.github.com>
2026-01-16 14:02:22 +1000

    Prepared for WP.org review - Renamed to Safe Activation & Timer

--- plugin-timer.php
+++ plugin-timer.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name:       Plugin Tester and Activation Timer
- * Plugin URI:        https://github.com/YOUR_GITHUB_USERNAME/plugin-timer
+ * Plugin Name:       Safe Activation & Timer
+ * Plugin URI:        https://github.com/prabin15/plugin-timer
  * Description:       Safely activate plugins with Pre-Flight Crash Checks and Automatic Timers.
  * Version:           2.2.0
  * Author:            Your Name
@@ -17,13 +17,6 @@ define( 'PT_PATH', plugin_dir_path( __FILE__ ) );
 
 require_once PT_PATH . 'includes/class-engine.php';
 require_once PT_PATH . 'includes/class-dashboard.php';
-
-// LOAD UPDATER
-if ( is_admin() ) {
-    require_once PT_PATH . 'includes/class-github-updater.php';
-    // REPLACE 'yourusername' and 'plugin-timer' with your actual info!
-    new PT_GitHub_Updater( __FILE__, 'yourusername', 'plugin-timer' );
-}
 
 new PT_Engine();
 new PT_Dashboard();
